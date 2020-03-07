eb752834656c55e14b6f4ec7f2085bea8ad7aa67
zeroxx1986 <zeroxx1986@gmail.com>
2020-03-07 13:28:49 +0100

    Update safaribooks.py
    multi to single for CSS & images collection on linux also.

--- safaribooks.py
+++ safaribooks.py
@@ -880,7 +880,9 @@ class SafariBooks:
                 self._thread_download_css(css_url)
 
         else:
-            self._start_multiprocessing(self._thread_download_css, self.css)
+            for css_url in self.css:
+                self._thread_download_css(css_url)
+            #self._start_multiprocessing(self._thread_download_css, self.css)
 
     def collect_images(self):
         if self.display.book_ad_info == 2:
@@ -897,7 +899,9 @@ class SafariBooks:
                 self._thread_download_images(image_url)
 
         else:
-            self._start_multiprocessing(self._thread_download_images, self.images)
+            for image_url in self.images:
+                self._thread_download_images(image_url)
+            #self._start_multiprocessing(self._thread_download_images, self.images)
 
     def create_content_opf(self):
         self.css = next(os.walk(self.css_path))[2]
